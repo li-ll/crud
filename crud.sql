b6aa40e45354d985afbddf9eae389b5328effba4
li-ll <58648337+li-ll@users.noreply.github.com>
2023-01-29 18:29:49 +0800

    Add files via upload
    数据库文件

---
+++ crud.sql
@@ -0,0 +1,44 @@
+/*
+SQLyog Professional v12.14 (64 bit)
+MySQL - 5.7.18-log : Database - pro
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`pro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
+
+USE `pro`;
+
+/*Table structure for table `tab_user` */
+
+DROP TABLE IF EXISTS `tab_user`;
+
+CREATE TABLE `tab_user` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `status` char(1) DEFAULT NULL,
+  `code` varchar(50) DEFAULT NULL,
+  `isDelete` varchar(1) DEFAULT '0',
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tab_user` */
+
+insert  into `tab_user`(`uid`,`username`,`password`,`email`,`status`,`code`,`isDelete`) values 
+(47,'ll','123456','2569750872@qq.com','N','cbea1ce4892b43acaa89a1cb3db14c2a','1'),
+(49,'ll2','1234567','1','N','2a7a97777fa247438c3f8f610efeab84','0'),
+(51,'ll3','123456','2569750872@qq.com','Y','e065ef5944894d31b47794a06ca724a1','1');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
